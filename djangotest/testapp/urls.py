98350b5306927b0619aae58f8c342f65f8f03c51
afedorov-advestis <afedorov@advestis.com>
2022-09-21 18:40:13 +0200

    new: dev: static_root added

--- djangotest/testapp/urls.py
+++ djangotest/testapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -22,3 +24,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
